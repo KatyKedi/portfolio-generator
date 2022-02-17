6152b24dbb22db68c9591ceb419f0c2d1971c55a
KatyKedi <katykat5bros@gmail.com>
2022-02-16 22:56:51 -0800

    generate the HTML file

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-        console.log(profileDataArr[i]); 
-    }
-    console.log("===============");
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};  
+const profileDataArgs = process.argv.slice(2);
 
-printProfileData(profileDataArgs);
+const [name, github] = profileDataArgs;
+
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
+
+    console.log('Portfolio complete! Check out index.html to see the output!')
+});
